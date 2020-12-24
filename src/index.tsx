c03dcf349206418b99eadc040cefdd6b64f676be
Sam Parsons <samparsons269@gmail.com>
2020-12-24 12:45:18 -0800

    initial demo prepublish

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,39 @@
 import React from 'react';
 import Dash from 'dashjs';
 
-class ReactDASH extends React.Component {
+interface Props {
+
+}
+
+interface State {
+  player: any,
+  url: string
+}
+
+class ReactDASH extends React.Component<Props, State> {
   constructor(props: any) {
     super(props);
-    console.log('made it to constructor')
+    this.state = {
+      player: null,
+      url: "https://dash.akamaized.net/envivio/EnvivioDash3/manifest.mpd"
+    }
   }
 
   componentDidMount() {
     console.log('CDM')
     console.log(Dash);
+    const player = Dash.MediaPlayer().create();
+    this.setState({player}, () => {
+      return player.initialize(document.querySelector("#videoPlayer")! as HTMLElement, this.state.url, true);
+    })
+
   }
 
   render() {
     return (
-      <div>This will be a component soon.</div>
+      <div>
+        <video id="videoPlayer" ></video>
+      </div>
     )
   }
 }
