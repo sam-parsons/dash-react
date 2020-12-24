e247a869760789c33e7d6a21b8b0a204df9d5147
Sam Parsons <samparsons269@gmail.com>
2020-12-24 09:34:59 -0800

    prelim code

---
+++ src/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class ReactDASH extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log('made it to constructor')
+  }
+
+  componentDidMount() {
+    console.log('CDM')
+  }
+
+  render() {
+    return (
+      <div>This will be a component soon.</div>
+    )
+  }
+}
+
+export default ReactDASH;
