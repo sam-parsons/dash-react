66c856f48b3fe80e7764c593fdf31911d686ecbe
Sam Parsons <samparsons269@gmail.com>
2020-12-27 20:58:41 -0800

    refactored tests and added two more

--- __tests__/DASHReact.tsx
+++ __tests__/DASHReact.tsx
@@ -3,15 +3,29 @@ import DASH from '../src/index';
 import { render, screen } from '@testing-library/react';
 
 describe('DASHReact', () => {
+  
   it('renders', () => {
     render(<DASH url={"https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps.mpd"} />);
   });
 
   it('accepts options object that conforms to MediaPlayerSettingClass', () => {
-    render(<DASH options={{
-      streaming: { scheduleWhilePaused: true, liveCatchup: {} }
-    }} url={"https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps.mpd"} />)
+    render(
+      <DASH 
+        options={{
+          streaming: { scheduleWhilePaused: true, liveCatchup: {} }
+        }} 
+        url={"https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps.mpd"} 
+      />
+    );
     const element = screen.getByTestId('dash-react');
     // TODO...
-  })
+  });
+  
+  it('accepts a custom id and inititializes', () => {
+    // TODO...
+  });
+
+  it('destroys instance of MediaPlayerClass when component unmounts', () => {
+    // TODO...
+  });
 })
